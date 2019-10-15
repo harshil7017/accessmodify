a4a1105c18532c4490321adb1f537aeda5d0245f
user <harshilsheth@outlook.com>
2019-10-15 18:47:31 +0530

    first change

---
+++ src/accessmodify/Accessm.java
@@ -0,0 +1,19 @@
+package accessmodify;
+
+class Accessm {
+    
+	private void display()
+	{
+		
+		System.out.println("abcd");
+	}
+}
+class access{
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Accessm ac=new Accessm();
+		
+	}
+
+}
